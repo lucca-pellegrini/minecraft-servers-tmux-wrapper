420197a3c0c11657ae1219e814dea2c2a440a7b7
Lucca Pellegrini <lucca@verticordia.com>
2025-07-12 00:48:24 -0300

    config.rs: decrease initial timeout to 10 seconds

--- src/config.rs
+++ src/config.rs
@@ -9,7 +9,7 @@ pub static SRV_STARTUP_SCRIPT: &str = "./run";
 pub static TMUX_SESSION: &str = "fabric-servers";
 pub static PROXY_PORT: u16 = 25564; // Port where the main Velocity proxy will listen
 pub static BUFFER_TIMEOUT: u64 = 90; // How long to await for proxy to start listening
-pub static INIT_TIMEOUT: u64 = 25; // How long to wait for client connections before the servers start
+pub static INIT_TIMEOUT: u64 = 10; // How long to wait for client connections before the servers start
 
 // Global program state.
 pub static SERVERS: Mutex<Vec<String>> = Mutex::new(Vec::new());
